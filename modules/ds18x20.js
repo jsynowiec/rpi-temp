f1e7026658668068f3e2cf7e8599727073fdf7f7
Jakub Synowiec <jakub.synowiec@gmail.com>
2015-11-16 12:48:41 +0100

    Rewrite DS18X20 module in ES2015

--- modules/ds18x20.js
+++ modules/ds18x20.js
@@ -1,67 +1,44 @@
-(function () {
-  'use strict';
+import Promise from 'bluebird';
+import fs from 'fs';
 
-  var Promise = require('bluebird'),
-      readFile = Promise.promisify(require('fs').readFile);
+const readFile = Promise.promisify(fs.readFile);
 
-  var BASE_DIRECTORY = '/sys/bus/w1/devices',
-      SLAVE_FILE = 'w1_slave',
-      SENSORS = {
-        DS18S20: 10,
-        DS1822: 22,
-        DS18B20: 28
-      };
-
-  module.exports = {
-    THERM_SENSOR_DS18S20: 'DS18S20',
-    THERM_SENSOR_DS1822: 'DS1822',
-    THERM_SENSOR_DS18B20: 'DS18B20',
-
-    /**
-     * DS18x20 sensor class.
-     *
-     * @param {string} deviceType
-     * @param {string} deviceId
-     * @constructor
-     */
-    Sensor: function (deviceType, deviceId) {
-      function getTemp(str) {
-        return /(?:t=)(\d+)/.exec(str)[1];
-      }
-
-      function parseTemp(t) {
-        return Number(t * 0.001);
-      }
-
-      /**
-       * Returns this sensor type, eg. DS18B20.
-       *
-       * @returns {String}
-       */
-      this.getType = function () {
-        return deviceType;
-      };
-
-      /**
-       * Returns this sensor hwId, eg. 0000062f17e2
-       *
-       * @returns {String}
-       */
-      this.getId = function () {
-        return deviceId;
-      };
-
-      /**
-       * Reads current temperature from the sensor.
-       *
-       * @returns {Promise}
-       */
-      this.getTemp = function () {
-        return readFile(BASE_DIRECTORY + '/' + SENSORS[deviceType] + '-' + deviceId + '/' + SLAVE_FILE)
-          .then(getTemp)
-          .then(parseTemp);
-      };
-    }
+const BASE_DIRECTORY = '/sys/bus/w1/devices',
+  SLAVE_FILE = 'w1_slave',
+  SENSORS = {
+    DS18S20: 10,
+    DS1822:  22,
+    DS18B20: 28
   };
 
-})();
+export const SENSOR_TYPES = {
+  THERM_SENSOR_DS18S20: 'DS18S20',
+  THERM_SENSOR_DS1822:  'DS1822',
+  THERM_SENSOR_DS18B20: 'DS18B20'
+};
+
+export default class Sensor {
+  constructor (deviceType, deviceId) {
+    this.deviceType = deviceType;
+    this.deviceId = deviceId;
+  }
+
+  get type () {
+    return this.deviceType;
+  }
+
+  get id () {
+    return this.deviceId;
+  }
+
+  /**
+   * Reads current temperature from the sensor.
+   *
+   * @returns {Promise}
+   */
+  getTemp () {
+    return readFile(BASE_DIRECTORY + '/' + SENSORS[this.deviceType] + '-' + this.deviceId + '/' + SLAVE_FILE)
+      .then((str) => /(?:t=)(\d+)/.exec(str)[1])
+      .then((t) => Number(t * 0.001));
+  }
+}
